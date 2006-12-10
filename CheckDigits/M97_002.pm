a6455e0547e24074bb76b3ead4e7929f2ee5c783
Mathias Weidner <mamawe@cpan.org>
2006-12-10 22:48:21 +0000

    Import of MAMAWE/Algorithm-CheckDigits-0.43 from CPAN.
    gitpan-cpan-distribution: Algorithm-CheckDigits
    gitpan-cpan-version:      0.43
    gitpan-cpan-path:         MAMAWE/Algorithm-CheckDigits-0.43.tar.gz
    gitpan-cpan-author:       MAMAWE
    gitpan-cpan-maturity:     released

--- CheckDigits/M97_002.pm
+++ CheckDigits/M97_002.pm
@@ -1,77 +1,115 @@
 package Algorithm::CheckDigits::M97_002;
+# vim: set sw=4 ts=4 tw=78 et si:
 
 use 5.006;
 use strict;
 use warnings;
 use integer;
 
-use Math::BigInt;
+#use Math::BigInt;
 
 our @ISA = qw(Algorithm::CheckDigits);
 
 my %subst = (
-	A => 10, B => 11, C => 12, D => 13, E => 14,
-	F => 15, G => 16, H => 17, I => 18, J => 19,
-	K => 20, L => 21, M => 22, N => 23, O => 24,
-	P => 25, Q => 26, R => 27, S => 28, T => 29,
-	U => 30, V => 31, W => 32, X => 33, Y => 34,
-	Z => 35,
+    A => 10, B => 11, C => 12, D => 13, E => 14,
+    F => 15, G => 16, H => 17, I => 18, J => 19,
+    K => 20, L => 21, M => 22, N => 23, O => 24,
+    P => 25, Q => 26, R => 27, S => 28, T => 29,
+    U => 30, V => 31, W => 32, X => 33, Y => 34,
+    Z => 35,
 );
 
 sub new {
-	my $proto = shift;
-	my $type  = shift;
-	my $class = ref($proto) || $proto;
-	my $self  = bless({}, $class);
-	$self->{type} = lc($type);
-	return $self;
+    my $proto = shift;
+    my $type  = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = bless({}, $class);
+    $self->{type} = lc($type);
+    return $self;
 } # new()
 
 sub is_valid {
-	my ($self,$number) = @_;
-	if ($number =~ /^[A-Z]{2}(\d\d)\s?[\d ]{0,37}$/i) {
-		return $1 eq $self->_compute_checkdigit($number);
-	}
+    my $self = shift;
+
+    if (my ($checkdigit,$number) = _prepare_number(shift)) {
+
+        return $checkdigit eq _compute_checkdigit($number);
+    }
 	return ''
 } # is_valid()
 
 sub complete {
-	my ($self,$number) = @_;
-	if ($number =~ /^([A-Z]{2})(00)(\s?[\d ]{0,37})$/i) {
-		return $1 . $self->_compute_checkdigit($number) . $3;
-	}
-	return '';
+    my $self = shift;
+    my $incomplete = uc(shift);
+
+    if (my ($checkdigit,$number) = _prepare_number($incomplete)) {
+
+        $incomplete =~ /^(..)..(.+)/;
+
+		return $1 . _compute_checkdigit($number) . $2;
+    }
+    return '';
 } # complete()
 
 sub basenumber {
-	my ($self,$number) = @_;
-	if ($number =~ /^([A-Z]{2})(\d\d)(\s?[\d ]{0,37})$/i) {
-		return $1.'00'.$3 if ($2 eq $self->_compute_checkdigit($number));
-	}
-	return '';
+    my $self = shift;
+    my $unchecked = shift;
+
+    if (my ($checkdigit,$number) = _prepare_number($unchecked)) {
+
+        $unchecked =~ /^(..)..(.+)/;
+
+		return $1.'00'.$2
+            if ($checkdigit eq _compute_checkdigit($number));
+    }
+    return '';
 } # basenumber()
 
 sub checkdigit {
-	my ($self,$number) = @_;
-	if ($number =~ /^([A-Z]{2})(\d\d)(\s?[\d ]{0,37})$/i) {
-		return $2 if ($2 eq $self->_compute_checkdigit($number));
-	}
-	return '';
+    my $self = shift;
+
+    if (my ($checkdigit,$number) = _prepare_number(shift)) {
+		return $checkdigit
+            if ($checkdigit eq _compute_checkdigit($number));
+    }
+    return '';
 } # checkdigit()
 
 sub _compute_checkdigit {
-	my $self   = shift;
-	my $number = shift;
+    my $number = shift;
 
-	$number =~ s/\s//g;
+#    my $bignum = Math::BigInt->new($number);
+#    my $mod    = $bignum % 97;
+#
+# A comparison with Benchmark::compthese() brought:
+#
+#             Rate   bignum 9_digits
+# bignum    2502/s       --     -95%
+# 9_digits 46225/s    1748%       --
+#
+# so I reverted _compute_checkdigit to this code.
+# Thanks to Detlef Pilzecker for making me aware of this.
 
-	if ($number =~ /^([A-Z])([A-Z])..([ \d]{0,37})$/i) {
-		my $bignum = Math::BigInt->new($3.$subst{$1}.$subst{$2}.'00');
-		my $mod    = $bignum % 97;
-		return sprintf("%2.2d",(98 - $mod));
-	}
-	return -1;
+    my $mod = '';
+    while ($number ne '') {
+        $number = $mod . $number;
+        $mod    = substr($number,0,9,'') % 97;
+    }
+    return sprintf("%02d",(98 - $mod));
 } # _compute_checkdigit()
+
+sub _prepare_number {
+    my $number = uc(shift);
+
+    if ($number =~ /^([A-Z]{2})(\d\d)([A-Z\d ]+)$/) {
+        my $checkdigit = $2;
+        $number        = $3 . $1 . '00';
+        $number        =~ s/\s//g;
+        $number        =~ s/([A-Z])/$subst{$1}/g;
+        return ($checkdigit,$number);
+    }
+    return;
+} # _prepare_number()
 
 # Preloaded methods go here.
 
@@ -116,7 +154,7 @@ according to the substitute table and the checksum is set to '00'.
 
 =item 1
 
-The whole number (without checksum) is taken modulo 97.
+The whole number is taken modulo 97.
 
 =item 2
 
@@ -125,7 +163,7 @@ The checksum is difference between 98 and the sum of step 2.
 =item 3
 
 If the checksum is smaller then 10, a leading zero will be
-supplemented.
+prepended.
 
 =back
 
@@ -172,10 +210,17 @@ None by default.
 
 Mathias Weidner, E<lt>mathias@weidner.in-bad-schmiedeberg.deE<gt>
 
+=head1 THANKS
+
+Detlef Pilzecker pointed out to me that there may be more letters as the first
+two in an IBAN number. He also made me aware of a faster method to compute the
+check number than using Math::BigInt.
+
 =head1 SEE ALSO
 
 L<perl>,
 L<CheckDigits>,
-F<www.pruefziffernberechnung.de>.
+F<www.pruefziffernberechnung.de>,
+F<www.sic.ch/en/tkicch_home/tkicch_standardization/tkicch_financialinstitutions_ibanipi.htm>.
 
 =cut
